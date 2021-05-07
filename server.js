845f943cf5955f4e9926e8b1008dad620fc31d28
Laura Heimann <hello@laura.media>
2021-05-07 15:45:34 +0200

    fixed devcheck

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ const forceSecure = require('express-force-https');
 
 let isDev = () => process.env.NODE_ENV || 'development' === 'development';
 
-console.log(process.env.NODE_ENV);
-console.log(isDev);
-
 const app = express();
 
 // Certificate
-if(!isDev) {
+if(!isDev()) {
     const sslPK = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/privkey.pem', 'utf8');
     const sslCert = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/cert.pem', 'utf8');
     const sslCA = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/chain.pem', 'utf8');
@@ -38,7 +35,7 @@ app.get('/discord', (req, res) => {
 app.use(express.static('public', { dotfiles: 'allow' }));
 
 // HTTP to HTTPS redirect
-if(!isDev) {
+if(!isDev()) {
     app.use(forceSecure);
 }
 
@@ -48,7 +45,7 @@ httpServer.listen(80, () => {
     console.log(`[mgg-comingsoon] HTTP server running.`);
 });
 
-if(!isDev) {
+if(!isDev()) {
     const httpsServer = https.createServer(credentials, app);
     httpsServer.listen(443, () => {
         console.log(`[mgg-comingsoon] HTTPS server running.`);
