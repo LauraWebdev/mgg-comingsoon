3efc0fd01c84626237294a8f97b89fffafa3b74f
Laura Heimann <hello@laura.media>
2021-05-07 15:28:13 +0200

    added ssl support

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const app = express();
-const port = 80;
 
+// Certificate
+const sslPK = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/privkey.pem', 'utf8');
+const sslCert = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/cert.pem', 'utf8');
+const sslCA = fs.readFileSync('/etc/letsencrypt/live/mygarage.games/chain.pem', 'utf8');
+const credentials = {
+    key: sslPK,
+    cert: sslCert,
+    ca: sslCA
+};
+
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.htm'));
 });
@@ -13,6 +26,13 @@ app.get('/discord', (req, res) => {
 
 app.use(express.static('public', { dotfiles: 'allow' }));
 
-app.listen(port, () => {
-    console.log(`[mgg-comingsoon] Server running on port ${port}`);
-})
+// Servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+    console.log(`[mgg-comingsoon] HTTP server running.`);
+});
+httpsServer.listen(443, () => {
+    console.log(`[mgg-comingsoon] HTTPS server running.`);
+});
