1cd9233b9f5c11fd27610b98dc6d1307a502c053
Laura Heimann <hello@laura.media>
2021-05-07 14:44:06 +0200

    add simple server

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.htm'));
+});
+
+app.get('/discord', (req, res) => {
+    res.redirect("https://discord.gg/vcr7Fk6MT6");
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`[mgg-comingsoon] Server running on port ${port}`);
+})
